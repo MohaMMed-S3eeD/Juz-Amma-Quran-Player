ed4eb48a573874b5a11c18fe53394d373c373ea1
Your Name <your-email@example.com>
2025-02-17 09:35:25 +0200

    Customize the size of the video container screen

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,11 @@
+const VideoPlayer = ({ /* ...props */ }) => {
+  return (
+    <div className="video-container">
+      <div className="relative h-full w-full">
+        {/* ...existing video player code... */}
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayer;
